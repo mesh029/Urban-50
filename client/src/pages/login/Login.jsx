8247abbd22a6b20fff40105aaa0b73fe3b5ecb77
mesh029 <oariri429@gmail.com>
2022-04-04 19:25:49 +0300

    twentieth

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
-      const res = await axios.post("http://localhost:5000/auth/login", {
+      const res = await axios.post("https://wubbachess.herokuapp.com/auth/login", {
         username: userRef.current.value,
         password: passwordRef.current.value,
       });
--- client/src/pages/settings/Settings.jsx
+++ client/src/pages/settings/Settings.jsx
@@ -12,7 +12,7 @@ export default function Settings() {
   const [success, setSuccess] = useState(false);
 
   const { user, dispatch } = useContext(Context);
-  const PF = "http://localhost:5000/images/"
+  const PF = "https://wubbachess.herokuapp.com/images/"
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -34,7 +34,7 @@ export default function Settings() {
       } catch (err) {}
     }
     try {
-      const res = await axios.put("http://localhost:5000/users/" + user._id, updatedUser);
+      const res = await axios.put("https://wubbachess.herokuapp.com/users/" + user._id, updatedUser);
       setSuccess(true);
       dispatch({ type: "UPDATE_SUCCESS", payload: res.data });
     } catch (err) {
