75406a6f533f85a72f687f63f6b4e5ded63d882a
mesh029 <oariri429@gmail.com>
2022-04-04 18:12:05 +0300

    twentieth

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ if (NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/client/build")));
 
     app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "clint", "build", "index.html"));
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 
 }
